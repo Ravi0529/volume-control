7e78d277e3535b984b2a8b5cb17c4cf408d495c2
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-04-03 22:49:30 +0530

    finger tips tracking for volume

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -1,7 +1,9 @@
 import sys
+
 sys.path.append(r"D:\VScode\Python\02_Projects\02_Hand_Tracking")
 import cv2
 import time
+import math
 import HandTrackingModule as htm
 
 #####################
@@ -19,7 +21,21 @@ while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
-    print(lmList)
+    if len(lmList) != 0:
+        # print(lmList[4], lmList[8])
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        cv2.circle(img, (x1, y1), 7, (0, 255, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 7, (0, 255, 0), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.circle(img, (cx, cy), 7, (0, 255, 0), cv2.FILLED)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+        # print(length) # min - 10, max - 150
+        if length < 20:
+            cv2.circle(img, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
